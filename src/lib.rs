d8a7fa3303a1b574c4b2aa66e679177cda1f0fc5
Sam Whited <sam@samwhited.com>
2017-05-03 18:06:40 -0500

    Bump version to 0.2.4

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@
 #![deny(missing_docs)]
 #![feature(try_from)]
 
-#![doc(html_root_url = "https://docs.rs/xmpp-jid/0.2.3")]
+#![doc(html_root_url = "https://docs.rs/xmpp-jid/0.2.4")]
 
 extern crate idna;
 
