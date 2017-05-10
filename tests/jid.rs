51ea05db53433e2110f4c5211a219433cd2c8fa2
Sam Whited <sam@samwhited.com>
2017-05-09 20:39:55 -0500

    Relicense using MIT/Apache-2.0

--- tests/jid.rs
+++ tests/jid.rs
@@ -1,3 +1,11 @@
+// Copyright 2017 The Mellium Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 #![cfg_attr(not(feature = "stable"), feature(try_from))]
 
 extern crate xmpp_jid;
